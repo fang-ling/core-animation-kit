67cd7761f083dada49083505c8f80b22042e8216
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-05-31 18:33:27 +0800

    Add CoreAnimationButtonLayer

--- Sources/CoreAnimationKit/CoreAnimationButtonLayer.m
+++ Sources/CoreAnimationKit/CoreAnimationButtonLayer.m
@@ -1,8 +1,8 @@
 /*
- *  CoreAnimationKit.h
+ *  CoreAnimationButtonLayer.m
  *  core-animation-kit
  *
- *  Created by Fang Ling on 2026/5/3.
+ *  Created by Fang Ling on 2026/5/31.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,14 +17,24 @@
  *  limitations under the License.
  */
 
-#ifndef CoreAnimationKit_h
-#define CoreAnimationKit_h
+#import "CoreAnimationButtonLayer.h"
 
-#import "../../CoreAnimationButtonLayer.h"
-#import "../../CoreAnimationDivisionLayer.h"
-#import "../../CoreAnimationLayer.h"
-#import "../../CoreAnimationLayerDelegate.h"
-#import "../../CoreAnimationParagraphLayer.h"
-#import "../../CoreAnimationTransaction.h"
+#import <JavaScriptCoreKit/JavaScriptCoreKit.h>
 
-#endif /* CoreAnimationKit_h */
+C_ASSUME_NONNULL_BEGIN
+
+@implementation CoreAnimationButtonLayer
+
+- (instancetype)init {
+  if (!(self = [super init])) {
+    return nil;
+  }
+
+  self.contents = [JavaScriptCoreContext makeButtonNode];
+
+  return self;
+}
+
+@end
+
+C_ASSUME_NONNULL_END
